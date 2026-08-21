5caab89cd68af291f72335de7c044ef21f7d32c4
AllanSTrab <pekagamestrabbold@gmail.com>
2026-08-21 08:11:43 -0300

    Cp01_02_0.2
    Deu certo vamooooo

--- Cp01_02_P/Assets/Script/ButtonFnaf.cs
+++ Cp01_02_P/Assets/Script/ButtonFnaf.cs
@@ -5,18 +5,43 @@ public class ButtonFnaf : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] GameObject buttonRight;
     [SerializeField] GameObject buttonLeft;
+    [SerializeField] GameObject buttonBackLeft;
+    [SerializeField] GameObject buttonBackRight;
 
     public void Awake()
     {
+        if(buttonBackLeft != null) buttonBackLeft.SetActive(false);
+        if (buttonBackRight != null) buttonBackRight.SetActive(false);
     }
     public void LeftSide()
     {
         animator.SetTrigger("leftTurn");
+        buttonBackLeft.SetActive(true);
+        buttonLeft.SetActive(false);
+        buttonRight.SetActive(false);
     }
 
     public void RightSide()
     {
         animator.SetTrigger("rightTurn");
-        //buttonLeftR.SetActive(true);
+        buttonBackRight.SetActive(true);
+        buttonLeft.SetActive(false);
+        buttonRight.SetActive(false);
+    }
+
+    public void BackLeft()
+    {
+        animator.SetTrigger("backLeft");
+        buttonBackLeft.SetActive(false);
+        buttonLeft.SetActive(true);
+        buttonRight.SetActive(true);
+    }
+
+    public void BackRight()
+    {
+        animator.SetTrigger("backRight");
+        buttonBackRight.SetActive(false);
+        buttonLeft.SetActive(true);
+        buttonRight.SetActive(true);
     }
 }
